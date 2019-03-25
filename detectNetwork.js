b2251530347104463f0b7022791ae2e5c1eaeb9e
Gregory Inouye <aznrunner@gmail.com>
2019-03-24 17:23:04 -0700

    detectNetwork.js to identify DC and AMEX

--- detectNetwork.js
+++ detectNetwork.js
@@ -13,6 +13,13 @@ var detectNetwork = function(cardNumber) {
   // The American Express network always starts with a 34 or 37 and is 15 digits long
 
   // Once you've read this, go ahead and try to implement this function, then return to the console.
+  if (cardNumber.length === 14 && (cardNumber.slice(0,2) === '38' || cardNumber.slice(0,2) === '39')) {
+  	return 'Diner\'s Club';
+  } else if (cardNumber.length === 15 && (cardNumber.slice(0,2) === '34' || cardNumber.slice(0,2) === '37')) {
+  	return 'American Express';
+  } else {
+  	return undefined;
+  }
 };
 
 
