4d3fcdbe6f400c764c702aec21ebfe45765480cd
Gregory Inouye <aznrunner@gmail.com>
2019-03-24 17:42:03 -0700

    detectNetwork.js Added Visa, MasterCard

--- detectNetwork.js
+++ detectNetwork.js
@@ -11,12 +11,20 @@ var detectNetwork = function(cardNumber) {
   // Note: `cardNumber` will always be a string
   // The Diner's Club network always starts with a 38 or 39 and is 14 digits long
   // The American Express network always starts with a 34 or 37 and is 15 digits long
+  // Visa always has a prefix of 4 and a length of 13, 16, or 19.
+  // MasterCard always has a prefix of 51, 52, 53, 54, or 55 and a length of 16.
 
   // Once you've read this, go ahead and try to implement this function, then return to the console.
-  if (cardNumber.length === 14 && (cardNumber.slice(0,2) === '38' || cardNumber.slice(0,2) === '39')) {
+  var numLength = cardNumber.length;
+  var numPrefix = cardNumber.slice(0, 2);
+  if (numLength === 14 && (numPrefix === '38' || numPrefix === '39')) {
   	return 'Diner\'s Club';
-  } else if (cardNumber.length === 15 && (cardNumber.slice(0,2) === '34' || cardNumber.slice(0,2) === '37')) {
+  } else if (numLength === 15 && (numPrefix === '34' || numPrefix === '37')) {
   	return 'American Express';
+  } else if ((numLength === 13 || numLength === 16 || numLength === 19) && (numPrefix[0] === '4')) {
+  	return 'Visa';
+  } else if ((numLength === 16) && (numPrefix === '51' || numPrefix === '52' || numPrefix === '53' || numPrefix === '54' || numPrefix === '55')) {
+  	return 'MasterCard';
   } else {
   	return undefined;
   }
