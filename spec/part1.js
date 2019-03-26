3f605df588ce2dc64f5b3adef465a990a3ca64c0
Gregory Inouye <aznrunner@gmail.com>
2019-03-25 21:11:47 -0700

    spec/part1.js modified test for _.uniq

--- spec/part1.js
+++ spec/part1.js
@@ -346,7 +346,12 @@
         var iterator = function(value) { return value === 1; };
         var numbers = [1, 2, 2, 3, 4, 4];
 
-        expect(_.uniq(FILL_ME_IN)).to.eql([1, 2]);
+        expect(_.uniq(_.uniq(numbers, true, iterator), false, function(value) {
+          if (value) {
+            return 1;
+          } else {
+            return 2;
+          }})).to.eql([1, 2]);
       });
 
       it('should produce a brand new array instead of modifying the input array', function() {
