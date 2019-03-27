ca78bac5c8e8cca133aa6bad504148c50e7feb5c
Gregory Inouye <aznrunner@gmail.com>
2019-03-26 22:33:13 -0700

    underbar.js completed _.extend, _.defaults

--- src/underbar.js
+++ src/underbar.js
@@ -235,11 +235,25 @@
   //     bla: "even more stuff"
   //   }); // obj1 now contains key1, key2, key3 and bla
   _.extend = function(obj) {
+  	for (var i = 1; i < arguments.length; i++) {
+  		for (var prop in arguments[i]) {
+  			obj[prop] = arguments[i][prop];
+  		}
+  	}
+  	return obj;
   };
 
   // Like extend, but doesn't ever overwrite a key that already
   // exists in obj
   _.defaults = function(obj) {
+  	for (var i = 1; i < arguments.length; i++) {
+  		for (var prop in arguments[i]) {
+  			if (obj[prop] === undefined) {
+  				obj[prop] = arguments[i][prop];
+  			}
+  		}
+  	}
+  	return obj;
   };
 
 
