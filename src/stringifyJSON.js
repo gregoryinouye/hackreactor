23c4cb9d719c689f39a21cf2818dc775e6a2724e
Gregory Inouye <aznrunner@gmail.com>
2019-03-30 08:48:29 -0700

    stringifyJSON.js conditionals set, framework set

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,6 +3,37 @@
 
 // but you don't so you're going to write it from scratch:
 
+// object: stringifyObject,
+// array: stringifyArray,
+// function: undefined,
+// symbol: undefined,
+// undefined: undefined
+// string: return string
+// boolean: toString
+// number: toString
+// null: typeof object --> return 'null'
+
 var stringifyJSON = function(obj) {
-  // your code goes here
+  if (typeof obj === 'object') {
+  	if (obj === null) {
+  		return 'null';
+  	} else if (Array.isArray(obj)) {
+  		// work on array
+  		// return '[' + stringifyJSON()
+  	} else {
+  		// work on object
+  	}
+  } else if (['boolean', 'number', 'string'].includes(typeof obj)) {
+  	return obj.toString();
+  } else if (['function', 'symbol', 'undefined'].includes(typeof obj)) {
+  	return undefined;
+  } 
 };
+
+var stringifyObject = function(obj) {
+  
+}
+
+var stringifyArray = function(arr) {
+
+}
