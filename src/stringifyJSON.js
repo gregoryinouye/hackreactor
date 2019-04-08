7f699c7db7a4d7321bab42e35c1b14b05bcea0d8
gregoryinouye <gregoryinouye@gmail.com>
2019-04-08 11:57:29 -0700

    Unstringifiable Tests

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,35 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  if (typeof obj === 'boolean' || typeof obj === 'number') {
+    return obj.toString();
+  } else if (obj === null) {
+    return '' + obj;
+  } else if (typeof obj === 'string') {
+    return '"' + obj + '"';
+  } else if (Array.isArray(obj)) {
+    let str = '[';
+    obj.forEach(function(element, idx) {
+      if (idx === obj.length - 1) {
+        str += stringifyJSON(element);
+      } else {
+        str += stringifyJSON(element) + ',';
+      }
+    });
+    return str + ']';
+  } else if (typeof obj === 'object') {
+    let str = '{';
+    let size = Object.keys(obj).length - 1;
+    for (let keys in obj) {
+      if (size === 0) {
+        str += '"' + keys + '":' + stringifyJSON(obj[keys]);
+    } else {
+        str += '"' + keys + '":' + stringifyJSON(obj[keys]) + ',';
+    }
+      size--;
+    }
+    return str + '}';
+  } else {
+    return '';
+  }
 };
