0410845d7dbaf2a18a56c4e7b308e7ae26d17156
gregoryinouye <gregoryinouye@gmail.com>
2019-04-08 16:13:45 -0700

    ESLinted getElementsByClassName

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -19,17 +19,13 @@
 //     // return output.concat(??, getElementsByClassName(??));
 // };
 
-
 var getElementsByClassName = function(className, node = document.body) {
-    let output = [];
-    if (node.classList.contains(className)) {
-        output.push(node);
-    }
-    for (let i = 0; i < node.children.length; i++) {
-       output = output.concat(getElementsByClassName(className, node.children[i]));
-    }
-  // recursive case: call get elements on all child nodes (node.children or childnodes?)
-  // base case: there are no child nodes --> identify if the desired class name is in element.classList
-    // return output.concat(??, getElementsByClassName(??));
-    return output;
+  let output = [];
+  if (node.classList.contains(className)) {
+    output.push(node);
+  }
+  for (let i = 0; i < node.children.length; i++) {
+    output = output.concat(getElementsByClassName(className, node.children[i]));
+  }
+  return output;
 };
