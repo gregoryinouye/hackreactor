050b97f829f5cae2022efebaa6c95455ac2b8031
gregoryinouye <gregoryinouye@gmail.com>
2019-04-08 15:19:09 -0700

    helper function final

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,41 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className, node = document.body) {
+    let output = [];
+    if (node.classList.contains(className)) output.push(node);
+    function recursiveHelper(className, node) {
+        for (let i = 0; i < node.children.length; i++) {
+            if (node.children[i].classList.contains(className)) {
+                output.push(node.children[i]);
+            }
+            recursiveHelper(className, node.children[i]);
+        }
+    }
+    recursiveHelper(className, node);
+    return output;
+  // recursive case: call get elements on all child nodes (node.children or childnodes?)
+  // base case: there are no child nodes --> identify if the desired class name is in element.classList
+    // return output.concat(??, getElementsByClassName(??));
 };
+
+
+// var getElementsByClassName = function(className, node = document.body) {
+//     let output = [];
+//     if (node.children.length === 0) {
+//         if (node.classList.contains(className)) {
+//             output.push(node);
+//         }
+//     } else {
+//         for (let i = 0; i < node.children.length; i++) {
+//             if (node.children[i].classList.contains(className)) {
+//                 output.push(node.children[i]);
+//             }
+//             output.push(getElementsByClassName(className, node.children[i]));
+//         }
+//     }   
+//     return output;
+//   // recursive case: call get elements on all child nodes (node.children or childnodes?)
+//   // base case: there are no child nodes --> identify if the desired class name is in element.classList
+//     // return output.concat(??, getElementsByClassName(??));
+// };
