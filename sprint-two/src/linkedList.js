1a353ba496e5711c0cdb5340ab583332c31003a8
gregoryinouye <gregoryinouye@gmail.com>
2019-04-11 14:11:39 -0700

    linkedList passes all tests

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -2,38 +2,35 @@ var LinkedList = function() {
   var list = {};
   list.head = null;
   list.tail = null;
-  list.index = 0;
 
   list.addToTail = function(value) {
     var newTail = Node(value);
-    newTail.index = list.index;
-    list[list.index] = newTail;
-    list.tail === null ? list.head = newTail : list[list.tail.index].next = newTail;
+    list.tail === null ? list.head = newTail : list.tail.next = newTail;
     list.tail = newTail;
-    list.index++;
   };
 
   list.removeHead = function() {
     // edge case: if only one item in list (tail = head), need to update list.tail
-    // if (list.head === null) {
-    //   return null;
-    // }
+    if (list.head === null) {
+      return null;
+    }
     var currentHead = list.head;
-    delete list[currentHead.index];
     list.head = currentHead.next;
     return currentHead.value;
   };
 
   list.contains = function(target) {
-    let keyList = Object.keys(list);
-    indexOnlyList = keyList.filter(function(element) {
-      return typeof element === 'number';
-    });
-    return _.some(indexOnlyList, function(element) {
-      return list[element].value === target;
-    });
+    var checker = function(node) {
+      if (node === null) {
+        return false;
+      } else if (node.value === target) {
+        return true;
+      } else {
+      return checker(node.next);
+      }
+    };
+    return checker(list.head);
   };
-
   return list;
 };
 
@@ -42,7 +39,6 @@ var Node = function(value) {
 
   node.value = value;
   node.next = null;
-  node.index = null;
 
   return node;
 };
