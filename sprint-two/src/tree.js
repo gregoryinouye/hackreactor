1202ec7adda098762aff33c8af23288b0fde3a1d
gregoryinouye <gregoryinouye@gmail.com>
2019-04-11 15:53:56 -0700

    tree passes all tests

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,23 +1,39 @@
 var Tree = function(value) {
   var newTree = {};
+  
   newTree.value = value;
-
-  // your code here
-  newTree.children = null;  // fix me
-
+  newTree.children = [];
+    // fix me
+  for (var key in treeMethods) {
+    newTree[key] = treeMethods[key];
+  }
   return newTree;
 };
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  var child = Tree(value);
+  this.children.push(child);
 };
 
 treeMethods.contains = function(target) {
+  var result = false;
+  var checker = function(tree) {
+    if (tree.value === target) {
+      result = true;
+    } else {
+      _.each(tree.children, checker);
+    }
+  };
+  checker(this);
+  return result;
 };
 
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ addChild() - constant O(1) time complexity
+ contains() - linear O(n) time complexity
  */
