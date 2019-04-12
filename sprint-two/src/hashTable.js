8f16a8d644c04f1d6a0b9b23b97cbb0d82bf5269
gregoryinouye <gregoryinouye@gmail.com>
2019-04-12 11:25:08 -0700

    hashTable.js wrote insert function, not yet tested

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,10 +7,16 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  if (this._storage.get(index) === undefined) {
+    this._storage.set(index, [k, v]);
+  } else {
+    this._storage.get(index).push([k, v]);
+  }
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  
 };
 
 HashTable.prototype.remove = function(k) {
