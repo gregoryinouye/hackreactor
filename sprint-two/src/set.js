610801d262d99b15dac7d71de7c44fe19c35928f
gregoryinouye <gregoryinouye@gmail.com>
2019-04-12 11:24:45 -0700

    set.js removed fixme comment

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,20 +1,26 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = {};
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  this._storage[item] = item;
 };
 
 setPrototype.contains = function(item) {
+  return this._storage[item] === item;
 };
 
 setPrototype.remove = function(item) {
+  delete this._storage[item];
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ .add = O(1)
+ .contains = O(1)
+ .remove = O(1)
  */
