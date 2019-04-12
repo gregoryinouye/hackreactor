b3ff2249d9f2e7fafbddcce43d8a4d6d6edb16db
gregoryinouye <gregoryinouye@gmail.com>
2019-04-12 15:49:21 -0700

    binarySearchTree.js passes all tests

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -14,12 +14,31 @@ var BinarySearchTree = function(value) {
 
 var binaryTreeMethods = {};
 
-binaryTreeMethods.insert = function(value) {
+binaryTreeMethods.insert = function(value, node = this) {
+  if (node.value > value) {
+    node.left === null ? node.left = BinarySearchTree(value) : node.insert(value, node.left);
+  } else if (node.value < value) {
+    node.right === null ? node.right = BinarySearchTree(value) : node.insert(value, node.right);
+  }
   // recursive case - there is a child in target direction
+  // base case - there is no child in target direction
+     // add newtree to target direction
   
 };
 
 binaryTreeMethods.contains = function(target) {
+  var result = false;
+  
+  this.depthFirstLog(function(value) {
+    if (target === value) {
+      result = true;
+    }
+  });
+  
+  return result;
+  
+  
+  
   // recursive - current node !== target, has child in target direction
   
   
@@ -30,16 +49,15 @@ binaryTreeMethods.contains = function(target) {
   
 };
 
-binaryTreeMethods.depthFirstLog = function(node, cb) {
+binaryTreeMethods.depthFirstLog = function(cb, node = this) {
   cb(node.value);
   if (node.left !== null) {
-    this.depthFirstLog(node.left, cb);
+    this.depthFirstLog(cb, node.left);
   }
   if (node.right !== null) {
-    this.depthFirstLog(node.right, cb);
+    this.depthFirstLog(cb, node.right);
   }
 };
-
 
 /*
  * Complexity: What is the time complexity of the above functions?
