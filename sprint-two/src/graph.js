9d009ab80f7ba24c199761d94c7c5383d9704c7d
gregoryinouye <gregoryinouye@gmail.com>
2019-04-11 19:56:57 -0700

    graph without edges functions or foreachnode

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -6,18 +6,35 @@ var Graph = function() {
 
 // Add a node to the graph, passing in the node's value.
 Graph.prototype.addNode = function(node) {
+  this[node] = {'value': node, 'edges': []};
 };
 
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
 Graph.prototype.contains = function(node) {
+  for (var key in this) {
+    if (this[key].value === node) {
+      return true;
+    }
+  }
+  return false;
 };
 
 // Removes a node from the graph.
 Graph.prototype.removeNode = function(node) {
+  delete this[node];
 };
 
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
 Graph.prototype.hasEdge = function(fromNode, toNode) {
+  // for (var i = 0; i < this[fromNode].edges.length; i++) {
+  //   if (this[fromNode].edges[i] === toNode) {
+  //     return true;
+  //   }
+  // }
+  // return false;
+  return _.some(this[fromNode].edges, function(element) {
+    return element === toNode;
+  });
 };
 
 // Connects two nodes in a graph by adding an edge between them.
