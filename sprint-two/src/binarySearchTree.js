2aeb16b73735ee2d4b5f3efbfada0536ab1b5484
gregoryinouye <gregoryinouye@gmail.com>
2019-04-12 15:26:46 -0700

    binarySearchTree prototype of depthFirstLog

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,4 +1,43 @@
 var BinarySearchTree = function(value) {
+  var newTree = {};
+  newTree.value = value;
+  
+  newTree.left = null;
+  newTree.right = null;
+  
+  for (var key in binaryTreeMethods) {
+    newTree[key] = binaryTreeMethods[key];
+  }
+  
+  return newTree;
+};
+
+var binaryTreeMethods = {};
+
+binaryTreeMethods.insert = function(value) {
+  // recursive case - there is a child in target direction
+  
+};
+
+binaryTreeMethods.contains = function(target) {
+  // recursive - current node !== target, has child in target direction
+  
+  
+  // base case is current node === target
+  
+  
+  // base !== target but there are no children
+  
+};
+
+binaryTreeMethods.depthFirstLog = function(node, cb) {
+  cb(node.value);
+  if (node.left !== null) {
+    this.depthFirstLog(node.left, cb);
+  }
+  if (node.right !== null) {
+    this.depthFirstLog(node.right, cb);
+  }
 };
 
 
