6cd4e22d6af2752db033168a7d646ab3092695b5
gregoryinouye <gregoryinouye@gmail.com>
2019-04-13 12:36:55 -0700

    hashTableHelpers - added time complexity

--- sprint-two/src/hashTableHelpers.js
+++ sprint-two/src/hashTableHelpers.js
@@ -56,4 +56,9 @@ var getIndexBelowMaxForKey = function(str, max) {
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ get() - constant O(1)
+ set() - constant O(1)
+ each() - linear O(n)
+ checkLimit() - constant O(1)
+ getIndexBelowMaxForKey() - linear O(n) with respect to the string
  */
