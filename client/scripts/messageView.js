02b82a5ec332954c937ca3401930dbb1bd9c795d
gregoryinouye <gregoryinouye@gmail.com>
2019-04-19 21:46:32 -0700

    updated class to actual username in messages

--- client/scripts/messageView.js
+++ client/scripts/messageView.js
@@ -2,7 +2,7 @@ var MessageView = {
 
   render: _.template(
       "<div class='chat'>" +
-        "<div class='username'><%- username %></div>" +
+        "<div class='<%- username %> username'><%- username %></div>" +
         "<div><%- text %></div>" +
         "<div><%- createdAt %></div>" +
       "</div>"
