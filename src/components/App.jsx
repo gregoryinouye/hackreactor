2c6f3871b7aa8f3e323ad688b30595db492215e7
gregoryinouye <gregoryinouye@gmail.com>
2019-04-22 21:38:19 -0700

    added todo comments for tomorrow for handleClick

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,26 +7,33 @@ class App extends React.Component {
 
   constructor(props) {
     super(props);
+    this.state = {
+      focusedVideo: exampleVideoData[0]
+    };
+    // handleClick()
+    // pass to VideoListEntry as a function
+    // event handler in videoListEntry so when it's clicked, it jumps back to App.js
+    // pass focusedVideo to videoPlayer
   }
 
   render() {
     return (
-    <div>
-      <nav className="navbar">
-        <div className="col-md-6 offset-md-3">
-          <div><Search/></div>
-        </div>
-      </nav>
-      <div className="row">
-        <div className="col-md-7">
-          <div><VideoPlayer video={exampleVideoData[0]}/></div>
-        </div>
-        <div className="col-md-5">
-          <div><VideoList videos={exampleVideoData}/></div>
+      <div>
+        <nav className="navbar">
+          <div className="col-md-6 offset-md-3">
+            <div><Search state={this.state}/></div>
+          </div>
+        </nav>
+        <div className="row">
+          <div className="col-md-7">
+            <div><VideoPlayer video={exampleVideoData[0]} state={this.state}/></div>
+          </div>
+          <div className="col-md-5">
+            <div><VideoList videos={exampleVideoData} state={this.state}/></div>
+          </div>
         </div>
       </div>
-    </div>
-    )
+    );
   }
 }
 
