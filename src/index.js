8bcad1343d9da803fa4357149cb08167c427f51d
gregoryinouye <gregoryinouye@gmail.com>
2019-04-25 12:32:13 -0700

    partial implementation of redux store

--- src/index.js
+++ src/index.js
@@ -3,13 +3,18 @@ import ReactDOM from 'react-dom';
 import App from './components/App.js';
 import searchYouTube from './lib/searchYouTube.js';
 import YOUTUBE_API_KEY from './config/youtube.js';
+import {Provider} from 'react-redux';
+import configureStore from './store/store.js';
 
 //TODO: Import the Provider component from 'react-redux' here!
 
 //TODO: Use the Provider component to make your store available to
 //  the rest of your app.
+const store = configureStore();
 
 ReactDOM.render(
-  <App API_KEY={YOUTUBE_API_KEY} searchYouTube={searchYouTube} />,
+  <Provider store={store}>
+    <App API_KEY={YOUTUBE_API_KEY} searchYouTube={searchYouTube}/>
+  </Provider>,
   document.getElementById('app')
 );
--- src/store/store.js
+++ src/store/store.js
@@ -4,3 +4,20 @@ import rootReducer from './../reducers/main.js';
 import exampleVideoData from '../data/exampleVideoData.js';
 
 // TODO:  Create your redux store, apply thunk as a middleware, and export it!
+// export default function configureStore(initialState = {currentVideo: null, videos: []}) {
+//   return createStore(
+//     rootReducer,
+//     initialState,
+//     applyMiddleware(thunk),
+//     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+//   );
+// }
+
+export default function configureStore(initialState = {currentVideo: null, videos: []}) {
+  return createStore(
+    rootReducer,
+    initialState,
+    applyMiddleware(thunk)
+    // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  );
+}
