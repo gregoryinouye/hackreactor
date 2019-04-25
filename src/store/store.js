288b39f76885355220db7c0aed7fa6d44d585f0f
gregoryinouye <gregoryinouye@gmail.com>
2019-04-25 14:43:39 -0700

    added redux store

--- src/store/store.js
+++ src/store/store.js
@@ -4,20 +4,12 @@ import rootReducer from './../reducers/main.js';
 import exampleVideoData from '../data/exampleVideoData.js';
 
 // TODO:  Create your redux store, apply thunk as a middleware, and export it!
-// export default function configureStore(initialState = {currentVideo: null, videos: []}) {
-//   return createStore(
-//     rootReducer,
-//     initialState,
-//     applyMiddleware(thunk),
-//     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-//   );
-// }
 
-export default function configureStore(initialState = {currentVideo: null, videos: []}) {
+export default (function configureStore(initialState = {currentVideo: null, videos: []}) {
   return createStore(
     rootReducer,
     initialState,
     applyMiddleware(thunk)
     // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   );
-}
+}());
