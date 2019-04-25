fee185415f35ae8c493fe63c59d752729e414287
gregoryinouye <gregoryinouye@gmail.com>
2019-04-25 12:32:59 -0700

    fixed index.js provider render

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './components/App.js';
 import searchYouTube from './lib/searchYouTube.js';
 import YOUTUBE_API_KEY from './config/youtube.js';
-import {Provider} from 'react-redux';
+import { Provider } from 'react-redux';
 import configureStore from './store/store.js';
 
 //TODO: Import the Provider component from 'react-redux' here!
