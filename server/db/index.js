249320494b513b0b6beab52973e19179c5b0b8e6
gregoryinouye <gregoryinouye@gmail.com>
2019-05-02 22:11:45 -0700

    Need to refactor models/index.js to use sequelize

--- server/db/index.js
+++ server/db/index.js
@@ -1,19 +1,43 @@
-var mysql = require('mysql');
+// var mysql = require('mysql');
+var Sequelize = require('sequelize');
+
+var db = new Sequelize('chat', 'student', 'student', {
+  dialect: 'mysql',
+  host: 'localhost',
+});
+
+db.authenticate()
+  .then(() => console.log('Connected to db'))
+  .catch((err) => console.log('Unable to connect')
+);
+
+var User = db.define('users', {
+  username: Sequelize.STRING
+});
+
+var Message = db.define('messages', {
+  user_id: Sequelize.INTEGER,
+  text: Sequelize.STRING,
+  roomname: Sequelize.STRING
+});
+  
+
+module.exports = db;
 
 // Create a database connection and export it from this file.
 // You will need to connect with the user "root", no password,
 // and to the database "chat".
 
-module.exports.dbConnection = mysql.createConnection({
-  user: 'student',
-  password: 'student',
-  database: 'chat'
-});
+// module.exports.dbConnection = mysql.createConnection({
+//   user: 'student',
+//   password: 'student',
+//   database: 'chat'
+// });
 
-module.exports.dbConnection.connect((err) => {
-  if (err) {
-    throw err;
-  } else {
-    console.log('Connected to db');
-  }
-});
+// module.exports.dbConnection.connect((err) => {
+//   if (err) {
+//     throw err;
+//   } else {
+//     console.log('Connected to db');
+//   }
+// });
--- server/models/index.js
+++ server/models/index.js
@@ -1,9 +1,12 @@
 var db = require('../db');
 
+
+
+
 module.exports = {
   messages: {
     get: function (callback) {
-      db.dbConnection.query('SELECT messages.text, messages.roomname, users.username FROM messages LEFT OUTER JOIN users ON users.id WHERE users.id = messages.user_id', (err, results) => {
+      db.query('SELECT messages.text, messages.roomname, users.username FROM messages LEFT OUTER JOIN users ON users.id WHERE users.id = messages.user_id', (err, results) => {
         if (err) {
           console.log(err);
         } else {
@@ -12,7 +15,7 @@ module.exports = {
       });
     },
     post: function (reqBody, callback) {
-      db.dbConnection.query(`INSERT INTO messages (text, user_id, roomname) VALUES('${reqBody.message}', (SELECT id from users WHERE username = '${reqBody.username}'), '${reqBody.roomname}')`, (err) => {
+      db.query(`INSERT INTO messages (text, user_id, roomname) VALUES('${reqBody.message}', (SELECT id from users WHERE username = '${reqBody.username}'), '${reqBody.roomname}')`, (err) => {
         if (err) {
           console.log('username not found');
         } else {
@@ -24,7 +27,7 @@ module.exports = {
 
   users: {
     get: function (callback) {
-      db.dbConnection.query('SELECT * FROM users', (err, results) => {
+      db.query('SELECT * FROM users', (err, results) => {
         if (err) {
           console.log(err);
         } else {
@@ -33,7 +36,7 @@ module.exports = {
       });
     },
     post: function (username, callback) {
-      db.dbConnection.query(`INSERT into users (username) VALUES('${username}')`, (err) => {
+      db.query(`INSERT into users (username) VALUES('${username}')`, (err) => {
         if (err) {
           console.log(err);
         } else {
