892742dd400d647d902237a8d0379d16aadfb334
gregoryinouye <gregoryinouye@gmail.com>
2019-05-06 15:36:24 -0700

    check for row wins and column wins

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,15 +1,15 @@
 // *** MODEL *** //
 
-let currentMove = 'X';
+let nextMove = 'X';
 let board = ['0,0', '0,1', '0,2', '1,0', '1,1', '1,2', '2,0', '2,1', '2,2'];
 let boardValues = new Array(9).fill('');
 let isGameFinished = false;
-let rowColDiagList = 
+let gameWinner = null;
 
 let checkGameStatus = () => {
-  if (!isGameFinished) {
+  // if (!isGameFinished) {
     
-  }
+  // }
 };
 
 let getBoardValues = () => {
@@ -23,31 +23,56 @@ let updateBoardValue = (boxId, value) => {
   boardValues[index] = value;
 };
 
-let checkColumnWin = ([row, column]) {
-
+let checkRowWin = ([row, column], lastMove) => {
+  let currRow = [0, 1, 2].map(element => boardValues[row * 3 + element]);
+  if (currRow.every(item => item === lastMove)) {
+    gameWinner = lastMove;
+    return isGameFinished = true;
+  } else {
+    return isGameFinished;
+  }
 }
 
-let checkRowWin = ([row, column]) {
-
+let checkColumnWin = ([row, column], lastMove) => {
+  let currCol = [0, 1, 2].map(element => boardValues[element * 3 + column]);
+  if (currCol.every(item => item === lastMove)) {
+    gameWinner = lastMove;
+    return isGameFinished = true;
+  } else {
+    return isGameFinished;
+  }
 }
 
-let checkDiagWin = ([row, column]) {
-  
+let checkDiagWin = (parsedId) => {
+
+  //check if row, column is on a diag
+  // if yes, then check for winner
+  // else return false
+}
+
+let checkAllWins = boxId => {
+  locArr = parseBoxId(boxId);
+  if (checkRowWin(locArr) || checkColumnWin(locArr) || checkDiagWin(locArr)) {
+    return gameWinner;
+  } else if (board) // board is full, return TIE
+    {
+      //do stuff
+  }
 }
 
 // *** CONTROLLER *** //
 
 let getNextMove = function() {
-  if (currentMove === 'X') {
-    currentMove = 'O';
+  if (nextMove === 'X') {
+    nextMove = 'O';
     return 'X';
   } else {
-    currentMove = 'X';
+    nextMove = 'X';
     return 'O';
   }
 };
 
-// let parseBoxId = string => positionArray = string.split(',');
+let parseBoxId = string => string.split(',');
 
 let addMove = function(boxId) {
   if (document.getElementById(boxId).innerHTML === '' && !isGameFinished) {
@@ -64,6 +89,7 @@ let resetBoard = function() {
   board.forEach(id => updateBox(id, ''))
   isGameFinished = false;
   boardValues = new Array(9).fill('');
+  gameWinner = null;
 };
 
 let updateBox = function(boxId, value) {
