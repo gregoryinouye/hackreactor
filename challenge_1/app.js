763484149682e76b34ab92e07f72f100ae2828f8
gregoryinouye <gregoryinouye@gmail.com>
2019-05-06 20:56:14 -0700

    refactor endGame actions into one function

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -29,46 +29,37 @@ let updateBoardValue = (boxId, value) => {
 let checkRowWin = ([row, column], lastMove) => {
   let currRow = [0, 1, 2].map(element => boardValues[Number(row) * 3 + Number(element)]);
   if (currRow.every(item => item === lastMove)) {
-    gameWinner = lastMove;
-    gameHistory[lastMove] += 1;
-    return isGameFinished = true;
-  } else {
-    return isGameFinished;
+    endGame(lastMove);
   }
 };
 
 let checkColumnWin = ([row, column], lastMove) => {
   let currCol = [0, 1, 2].map(element => boardValues[Number(element) * 3 + Number(column)]);
   if (currCol.every(item => item === lastMove)) {
-    gameWinner = lastMove;
-    gameHistory[lastMove] += 1;
-    return isGameFinished = true;
-  } else {
-    return isGameFinished;
+    endGame(lastMove);
   }
 };
 
 let checkDiagWin = (boxId, lastMove) => {
-  if (['0,0', '1,1', '2,2'].includes(boxId) && [0, 4, 8].every(item => boardValues[item] === lastMove)) {
-    gameWinner = lastMove;
-    return isGameFinished = true;
-  } else if (['0,2', '1,1', '2,0'].includes(boxId) && [2, 4, 6].every(item => boardValues[item] === lastMove)) {
-    gameWinner = lastMove;
-    gameHistory[lastMove] += 1;
-    return isGameFinished = true;
-  } else {
-    return isGameFinished;
+  if ((['0,0', '1,1', '2,2'].includes(boxId) && [0, 4, 8].every(item => boardValues[item] === lastMove)) ||
+  (['0,2', '1,1', '2,0'].includes(boxId) && [2, 4, 6].every(item => boardValues[item] === lastMove))) {
+    endGame(lastMove);
   }
 };
+
+let endGame = winner => {
+  gameWinner = winner;
+  gameHistory[winner] += 1;
+  updateScore();
+  return isGameFinished = true;
+}
 
 let checkAllWins = (boxId, lastMove) => {
   let locArr = parseBoxId(boxId);
   if (checkRowWin(locArr, lastMove) || checkColumnWin(locArr, lastMove) || checkDiagWin(boxId, lastMove)) {
-    console.log(gameWinner);
-    return gameWinner;
+    endGame(lastMove);
   } else if (boardValues.every(element => element !== '')) {
-    isGameFinished = true;
-    gameHistory.tie += 1;
+    endGame('tie');
   }
 };
 
@@ -117,5 +108,8 @@ let addPlayerName = (letter, userInput) => {
   if (letter === 'O') {
     playerO = userInput || 'Player';
   }
-  document.querySelector('.history').innerHTML = `${playerX} X: ${gameHistory.X} vs ${playerO} O: ${gameHistory.O} || Tie: ${gameHistory.tie}`;
+  updateScore();
 }
+
+let updateScore = () => document.querySelector('.history')
+  .innerHTML = `${playerX} X: ${gameHistory.X} vs ${playerO} O: ${gameHistory.O} || Tie: ${gameHistory.tie}`;
