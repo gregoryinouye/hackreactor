4d64e37b8808876da914c2aa7d12df7cbb68533b
gregoryinouye <gregoryinouye@gmail.com>
2019-05-08 14:05:15 -0700

    create initial setup of express server and checkout client

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,2 +1,35 @@
+// **** APP COMPONENT **** //
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      // formOne: false,
+      // formTwo: false,
+      // formThree: false
+    };
+  }
+
+  checkout() {
+    // e => {
+    //   e.preventDefault();
+    //   console.log('start checkout');
+    // }
+  }
+
+  render() {
+    return <div className="App">
+      <button onClick={this.checkout}>Checkout</button>
+    </div>
+  }
+}
+
+// **** RENDER **** //
+
+ReactDOM.render(
+  <App />, document.getElementById('app')
+);
 
 
+// import React from 'react';
+// import ReactDOM from 'react-dom';
--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,7 @@
+const express = require('express');
+const server = express();
+const port = 3000;
 
+server.listen(port, () => console.log(`Express server listening on port ${port}`));
 
+server.get('/', (req, res) => res.send('Hello World!'));
