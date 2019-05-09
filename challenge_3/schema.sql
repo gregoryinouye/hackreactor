3e7a4ea2dad0e99fd9fda8ac2f684da3d4548881
gregoryinouye <gregoryinouye@gmail.com>
2019-05-08 19:59:37 -0700

    allow post request to /purchase adds user data to sql database

---
+++ challenge_3/schema.sql
@@ -0,0 +1,21 @@
+USE multiformcheckout;
+
+DROP TABLE users;
+
+CREATE TABLE users
+(
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(50),
+  email VARCHAR(50),
+  password VARCHAR(50),
+  shippingAddress1 VARCHAR(50),
+  shippingAddress2 VARCHAR(50),
+  city VARCHAR(50),
+  zipcode INT,
+  phoneNumber INT,
+  creditCardNumber INT,
+  expirationDate VARCHAR(50),
+  cvv INT,
+  billingZipCode INT,
+  PRIMARY KEY (id)
+);
--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,11 +1,53 @@
+// **** EXPRESS **** //
+
 const express = require('express');
 const server = express();
 const port = 3000;
+
+const bodyParser = require('body-parser');
+const jsonParser = bodyParser.json();
 
 server.listen(port, () => console.log(`Express server listening on port ${port}`));
 
 server.use(express.static('public'));
 
-server.get('/', (req, res) => res.send('Hello World!'));
+server.post('/purchase', jsonParser, (req, res) => {
+  console.log('POST purchase');
+  addUser(req.body, console.log);
+})
 
-server.get('/checkout/formone', (req, res) => {});
+
+// **** MYSQL DATABASE **** //
+
+const mysql = require('mysql');
+const connection = mysql.createConnection({
+  user: 'student',
+  password: 'student',
+  database: 'multiformcheckout'
+});
+
+connection.connect(err => {
+  if (err) {
+    console.error('error connecting: ' + err);
+  } else {
+    console.log(`connected to db as id: ${connection.threadId}`);
+  }
+});
+
+
+// **** MODEL **** //
+
+
+
+// **** CONTROLLER **** //
+
+const addUser = (userData, cb) => {
+  connection.query('INSERT INTO users SET ?', userData, (err, data) => {
+    if (err) {
+      console.error(err);
+    } else {
+      cb(data.insertId);
+    }
+  });
+}
+
