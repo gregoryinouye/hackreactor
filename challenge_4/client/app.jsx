c1056015a89739933fd8bd52ee7b2373cd4bc50f
Gregory <gregoryinouye@gmail.com>
2019-08-06 12:33:40 -0700

    basic react, express, webpack, babel setup

--- challenge_4/client/app.jsx
+++ challenge_4/client/app.jsx
@@ -1,2 +1,17 @@
+import React from 'react';
 
+class App extends React.Component {
+  constructor(props) {
+    super(props);
 
+    this.state = {
+
+    };
+  }
+
+  render() {
+    return <div>TEST2</div>
+  }
+}
+
+export default App;
