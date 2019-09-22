ad56ddcbf8b973d77b8ebdb33505245067196283
Gregory Inouye <aznrunner@gmail.com>
2019-09-22 16:52:00 -0700

    display circle graphic on board

--- challenge_4/client/Square.jsx
+++ challenge_4/client/Square.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 
 function Square(props) {
-  return <td>Square</td>;
+  return <td>
+    <svg viewBox="0 0 10 10" xmlns="http://www.w3.org/2000/svg">
+      <rect x="0" y="0" width="100%" height="100%"/>
+      <circle cx="50%" cy="50%" r="4" fill="white"/>
+    </svg>
+  </td>;
 }
 
 export default Square;
