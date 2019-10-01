719068097661c3ea6d085660ae3b1506e939bd84
Gregory Inouye <aznrunner@gmail.com>
2019-10-01 16:07:25 -0700

    basic react/express setup

---
+++ challenge_b1/webpack.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  entry: './client/index.jsx',
+  output: {
+    path: `${__dirname}/public`,
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      }
+    ]
+  }
+};
