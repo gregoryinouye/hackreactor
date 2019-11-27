6f5c982812f20d65d982be6ae5cb008296b89086
Gregory Inouye <aznrunner@gmail.com>
2019-11-27 14:19:57 -0800

    revise null check for cell color

--- challenge_4/client/components/Square.jsx
+++ challenge_4/client/components/Square.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 
 const Square = (props) => {
   const { value, move } = props;
+  const color = value || 'white';
 
   return (
     <td className="square">
       <svg display="block" viewBox="0 0 10 10" xmlns="http://www.w3.org/2000/svg" >
        <rect x="0" y="0" width="100%" height="100%" />
-       <circle cx="50%" cy="50%" r="4" fill={value === null ? 'white' : value} onClick={move} />
+       <circle cx="50%" cy="50%" r="4" fill={color} onClick={move} />
      </svg>
     </td>
   );
