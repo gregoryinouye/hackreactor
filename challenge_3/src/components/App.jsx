85dc6bed977d730d5346c2dca0189a2d0b33577e
Gregory <gregoryinouye@gmail.com>
2019-12-25 23:43:06 -0800

    add styling for bowling scorebox

--- challenge_3/src/components/App.jsx
+++ challenge_3/src/components/App.jsx
@@ -13,10 +13,45 @@ class App extends React.Component {
         <h1>Bowling Score Calculator</h1>
         <br></br>
         <div className="game" style={{ display: "flex" }}>
-          <div className="frame">
-            <div>
-              extra pins
-            </div>
+          <div className="frame" style={{ display: "flex", flexDirection: "row-reverse", flexWrap: "wrap", width: "50px", height: "50px", outline: "1px solid" }}>
+            <div style={{ outline: "1px solid", width: "40%", height: "40%" }}></div>
+            <div style={{ outline: "1px solid", width: "40%", height: "40%" }}></div>
+          </div>
+          <div className="frame" style={{ display: "flex", flexDirection: "row-reverse", flexWrap: "wrap", width: "50px", height: "50px", outline: "1px solid" }}>
+            <div style={{ outline: "1px solid", width: "40%", height: "40%" }}></div>
+            <div style={{ outline: "1px solid", width: "40%", height: "40%" }}></div>
+          </div>
+          <div className="frame" style={{ display: "flex", flexDirection: "row-reverse", flexWrap: "wrap", width: "50px", height: "50px", outline: "1px solid" }}>
+            <div style={{ outline: "1px solid", width: "40%", height: "40%" }}></div>
+            <div style={{ outline: "1px solid", width: "40%", height: "40%" }}></div>
+          </div>
+          <div className="frame" style={{ display: "flex", flexDirection: "row-reverse", flexWrap: "wrap", width: "50px", height: "50px", outline: "1px solid" }}>
+            <div style={{ outline: "1px solid", width: "40%", height: "40%" }}></div>
+            <div style={{ outline: "1px solid", width: "40%", height: "40%" }}></div>
+          </div>
+          <div className="frame" style={{ display: "flex", flexDirection: "row-reverse", flexWrap: "wrap", width: "50px", height: "50px", outline: "1px solid" }}>
+            <div style={{ outline: "1px solid", width: "40%", height: "40%" }}></div>
+            <div style={{ outline: "1px solid", width: "40%", height: "40%" }}></div>
+          </div>
+          <div className="frame" style={{ display: "flex", flexDirection: "row-reverse", flexWrap: "wrap", width: "50px", height: "50px", outline: "1px solid" }}>
+            <div style={{ outline: "1px solid", width: "40%", height: "40%" }}></div>
+            <div style={{ outline: "1px solid", width: "40%", height: "40%" }}></div>
+          </div>
+          <div className="frame" style={{ display: "flex", flexDirection: "row-reverse", flexWrap: "wrap", width: "50px", height: "50px", outline: "1px solid" }}>
+            <div style={{ outline: "1px solid", width: "40%", height: "40%" }}></div>
+            <div style={{ outline: "1px solid", width: "40%", height: "40%" }}></div>
+          </div>
+          <div className="frame" style={{ display: "flex", flexDirection: "row-reverse", flexWrap: "wrap", width: "50px", height: "50px", outline: "1px solid" }}>
+            <div style={{ outline: "1px solid", width: "40%", height: "40%" }}></div>
+            <div style={{ outline: "1px solid", width: "40%", height: "40%" }}></div>
+          </div>
+          <div className="frame" style={{ display: "flex", flexDirection: "row-reverse", flexWrap: "wrap", width: "50px", height: "50px", outline: "1px solid" }}>
+            <div style={{ outline: "1px solid", width: "40%", height: "40%" }}></div>
+            <div style={{ outline: "1px solid", width: "40%", height: "40%" }}></div>
+          </div>
+          <div className="frame" style={{ display: "flex", flexDirection: "row-reverse", flexWrap: "wrap", width: "50px", height: "50px", outline: "1px solid" }}>
+            <div style={{ outline: "1px solid", width: "40%", height: "40%" }}></div>
+            <div style={{ outline: "1px solid", width: "40%", height: "40%" }}></div>
           </div>
         </div>
       </div>
