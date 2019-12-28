a606b7c77086a9f015814b5c9a023e13e7b2c445
Gregory <gregoryinouye@gmail.com>
2019-12-27 22:47:58 -0800

    save styling

--- challenge_3/src/components/Frame.jsx
+++ challenge_3/src/components/Frame.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 
+const ballScoreStyle = { outline: "1px solid", width: "40%", height: "40%" };
+const frameStyle = { display: "flex", flexDirection: "row-reverse", flexWrap: "wrap", width: "50px", height: "50px", outline: "1px solid" };
+
 const Frame = (props) => {
   return (
-    <div className="frame" style={{ display: "flex", flexDirection: "row-reverse", flexWrap: "wrap", width: "50px", height: "50px", outline: "1px solid" }}>
-      <div style={{ outline: "1px solid", width: "40%", height: "40%" }}></div>
-      <div style={{ outline: "1px solid", width: "40%", height: "40%" }}></div>
+    <div className="frame" style={frameStyle}>
+      <div style={ballScoreStyle}></div>
+      <div style={ballScoreStyle}></div>
     </div>
   );
 }
