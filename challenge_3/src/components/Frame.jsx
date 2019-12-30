c931c4ed913ecae09c01abe3a4eda7329869efa3
Gregory <gregoryinouye@gmail.com>
2019-12-30 09:35:17 -0800

    fix scoring calc

--- challenge_3/src/components/Frame.jsx
+++ challenge_3/src/components/Frame.jsx
@@ -4,10 +4,13 @@ const ballScoreStyle = { outline: "1px solid", width: "40%", height: "40%" };
 const frameStyle = { display: "flex", flexDirection: "row-reverse", flexWrap: "wrap", width: "50px", height: "50px", outline: "1px solid" };
 
 const Frame = (props) => {
+  const { ball1, ball2 } = props;
+
   return (
     <div className="frame" style={frameStyle}>
       <div style={ballScoreStyle}></div>
       <div style={ballScoreStyle}></div>
+      {ball1 + ball2}
     </div>
   );
 }
